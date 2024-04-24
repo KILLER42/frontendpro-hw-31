9153312395192cf247650f5e12c8643685abef0b
KILLER42 <40739278+KILLER42@users.noreply.github.com>
2024-04-25 02:50:13 +0300

    added homework files

---
+++ src/js/script.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const usedNumbers = [];
+const randomNumber = function () {
+    const randomNumberResult = Math.floor(Math.random() * 100);
+    if (usedNumbers.includes(randomNumberResult)) return randomNumber();
+    usedNumbers.push(randomNumberResult);
+    return randomNumberResult;
+}
+
+for (let i = 0; i <= 99; i += 1) {
+    console.log(randomNumber());
+}
+
+console.log(usedNumbers.sort())
